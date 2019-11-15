11d12ca3f5508431ea4099ad36d8e6ab2c08ce5d
OliverND <308404092@qq.com>
2019-11-15 23:30:48 +0800

    2019.11.15 添加 Eureka_Server & Eureka_Client

---
+++ eureka_server/src/test/java/xyz/nandu/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,14 @@
+package xyz.nandu.eureka_server;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
